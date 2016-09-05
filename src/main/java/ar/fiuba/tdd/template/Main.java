9307bc349b0f2d9fb7413d3cc39d6117383788fe
busiris <iggalvez2015@gmail.com>
2016-09-05 10:24:34 -0300

    preparing for Travis build

--- src/main/java/ar/fiuba/tdd/template/Main.java
+++ src/main/java/ar/fiuba/tdd/template/Main.java
@@ -1,7 +1,9 @@
 package ar.fiuba.tdd.template;
 
 public class Main {
+    private static String MESSAGE = "This is just a template project";
+
     public static void main(String[] args) {
-        System.out.println("This is just a template project");
+        System.out.println(MESSAGE);
     }
 }
